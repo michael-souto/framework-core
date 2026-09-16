14757d178a59fa417c58971ad308f1233789e19d
Michael Souto <michael.souto@detrasoft.com>
2026-09-16 18:59:30 -0300

    refactor: add NoArgsConstructor and AllArgsConstructor to ResponseNotification classes

--- src/main/java/com/detrasoft/framework/core/notification/TResponseNotification.java
+++ src/main/java/com/detrasoft/framework/core/notification/TResponseNotification.java
@@ -4,13 +4,17 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TResponseNotification<T> {
 	protected Instant timestamp;
 	protected Integer status;
